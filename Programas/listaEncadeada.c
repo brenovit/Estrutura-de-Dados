ee04a8eec18845bfa924fc2c3f22757ed1533885
Breno Nunes <breno.arantes97@gmail.com>
2016-05-10 10:18:59 -0300

    Liberar a memoria e Quantidade de Nós
    Adicionei 2 funções novas.
    Liberar todos os nós da lista da memoria, e
    Retornar a quantidade de Nós presente na lista.
    Todo o código esta comentado.

--- Programas/listaEncadeada.c
+++ Programas/listaEncadeada.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <locale.h>
 typedef int tp_item;
 
 typedef struct tp_no{                                   ///estrutura da lista,
@@ -11,23 +11,32 @@ typedef struct tp_no{                                   ///estrutura da lista,
 tplista* inicializa();                                  ///inicializar a lista
 int lista_vazia(tplista *lista);                        ///checar se a lista ta vazia
 tplista* aloca();                                       ///aloca memoria do tamanho de uma lista
-int inserir_final(tplista **lista, tp_item e);
-void listagem(tplista *lista);
+int inserir_final(tplista **lista, tp_item e);          ///esta função ira inserir um novo nó no final da lista
+void listagem(tplista *lista);                          ///esta função ira listar todos os elementos contidos na lsita
+void libera_lista(tplista **lista);                     ///esta função ira limpar toda lista(liberar o espaço em memoria)
+int tamanho_lista(tplista **lista);                     ///esta função ira retornar o tamanho da lista
 
 void main(){
+    setlocale(LC_ALL,"");
     tplista *lista;                                     ///criação de um ponteiro da lista
     tp_item e;
     lista = inicializa();                               ///inicializando a lista(seu valor será nulo)
+
+    inserir_final(&lista,10);
+    inserir_final(&lista,20);
+    inserir_final(&lista,30);
+
+    //libera_lista(&lista);
+
     if(lista_vazia(lista)){                             ///executa a função que checa se esta função se diferente de 0
         printf("A lista esta vazia\n");
     }else{                                              ///senão
         printf("A lista não esta vazia\n");
     }
-    inserir_final(&lista,10);
-    inserir_final(&lista,20);
-    inserir_final(&lista,30);
-
+    printf("\nQuantidade de nós presente na lista: %d\n\n",tamanho_lista(&lista));
     listagem(lista);
+
+
 }
 
 tplista *inicializa(){                                  ///esta função que é um ponteiro de lista
@@ -54,8 +63,8 @@ int inserir_final(tplista **lista, tp_item e){           ///esta função vair ins
     if(novo_no == NULL)                                  ///verifica se o ponteiro recebeu null, traduzindo, não foi possivel alocar memoria, (o malloc vai informar isso)
         return 0;                                        ///não conseguiu alocar memoria
 
-    atu = *lista;                                        ///o ponteiro ant vai receber o valor que o 'l' aponta , o 'l' vira um ponteiro de indireção, pois ponta para um ponteiro
-    ant = NULL;
+    atu = *lista;                                        ///o ponteiro atu vai receber o valor que a 'lista' aponta , a 'lista' vira um ponteiro de indireção, pois aponta para um ponteiro
+    ant = NULL;                                          ///o nó anteriro receber nulo
     novo_no->info = e;                                   ///o ponteiro que recebeu o endereço de memoria alocada vai salvar em info o valor inserido no parametro
     novo_no->prox = NULL;
 
@@ -63,20 +72,44 @@ int inserir_final(tplista **lista, tp_item e){           ///esta função vair ins
         ant = atu;                                       ///o ponteiro anterior vai apontar para o ponteiro atual
         atu = atu->prox;                                 ///o ponteiro atual vai apontar para o proximo nó que que ele aponta
     }
-    ///esta parte vai ser executada na primeira vez da lista.
-    if(ant == NULL){
-        *lista = novo_no;
+    ///esta parte vai ser executada na primeira vez que a lista for executada.
+    if(ant == NULL){                                     ///se o ponteiro anterior for nulo(quer dizer que ela esta vazia)
+        *lista = novo_no;                                ///o valor da lista, vai ser o novo nó criado
     }else{
         ant->prox = novo_no;
     }
     return 1;
 }
 
-void listagem(tplista *lista){
-    tplista *atu;
-    atu = lista;
-    while(atu != NULL){
-        printf("Valor na lista: %d\n",atu->info);
-        atu = atu->prox;
+void listagem(tplista *lista){                          ///esta função lista todos os elementos contido em uma lista
+    tplista *atu;                                       ///cria-se um ponteiro do tipo tplista
+    atu = lista;                                        ///este ponteiro vai receber a lista passada como parametro
+    while(atu != NULL){                                 ///laço que vai passaar por toda a lista, até o seu valor se tornar nulo
+        printf("Valor na lista: %d\n",atu->info);       ///mostra o dado(info) contido na lista
+        atu = atu->prox;                                ///diz para o ponteiro 'atu', que ele vai receber o nó, que o nó atual aponta
+    }
+}
+
+int tamanho_lista(tplista **lista){                     ///esta função ira retornar o tamanho da lista, percorrendo cada elemento
+    if(lista_vazia(lista))                              ///verifica se a lista esta vazia
+        return 0;
+    int cont = 0;                                       ///cria-se um contador
+    tplista *no = *lista;                               ///cria-se um nó, que recebe o primeiro nó da lista passado como parametro
+    while(no != NULL){                                  ///enquanto este nó não for nulo
+        cont++;                                         ///o contador aumenta
+        no = no->prox;                                  ///o nó vai apontar para o proximo nó
+    }
+    return cont;
+}
+
+void libera_lista(tplista **lista){                     ///esta função vai limpar toda a lista
+    tplista *no;                                        ///cria-se uma nova lista
+    if(!lista_vazia(lista)){                            ///verifica se a lista não é nula
+        while((*lista) != NULL){                        ///enquanto existir nó dentro da lista
+            no = *lista;                                ///o novo nó vai receber o primeiro nó da lista
+            *lista = (*lista)->prox;                    ///esta lista vai avançar para o nó que ela aponta
+            free(no);                                   ///libera a memoria do nó
+        }
+        free(lista);                                    ///por fim, a lista é liberada por completo da memoria
     }
 }
